00b61328e085a65bd89db74cb853c63b2c6ad24b
kaden king <kingk45@ufl.edu>
2020-10-05 14:02:53 -0400

    user can get all their projects

--- server/lib/server_web/controllers/project_controller.ex
+++ server/lib/server_web/controllers/project_controller.ex
@@ -1,11 +1,23 @@
 defmodule ServerWeb.ProjectController do
   use ServerWeb, :controller
   import Server.Projects.Project
+  import Ecto.Query
 
+  alias Server.Projects.Project
+  alias Server.Users.User
+
+  @spec index(Plug.Conn.t(), any) :: Plug.Conn.t()
   def index(conn, _params) do
-    send_resp(conn, 201, "hey from project controller")
+    %{id: user_id} = Pow.Plug.current_user(conn)
+
+    query = from(u in User, select: u, join: p in Project, on: u.id == p.user_id, where: u.id == ^user_id, preload: [project: p])
+
+    %User{project: projects} = Server.Repo.one(query)
+
+    json(conn, projects)
   end
 
+  @spec create(Plug.Conn.t(), map) :: Plug.Conn.t()
   def create(conn, %{"name" => name}) do
     user = Pow.Plug.current_user(conn)
     project = new_project_changeset(%{"name" => name, "user_id" => user.id})
