546825c7843887277f98c0db73643b823ae83408
Hubert Zhao <zhao.hubert@gmail.com>
2020-10-16 14:29:19 -0400

    separated out realtime code

--- example_project/src/App.js
+++ example_project/src/App.js
@@ -1,36 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-const { Socket } = require('phoenix-channels')
+import {useChannel} from './realtime.js'
+// const { Socket } = require('phoenix-channels')
  
-let socket = new Socket("ws://localhost:4000/socket")
+// let socket = new Socket("ws://localhost:4000/socket")
  
-socket.connect()
+// socket.connect()
  
 // Now that you are connected, you can join channels with a topic:
 
 
 
-const useChannel = (channelName) => {
-  const [channel, setChannel] = useState();
+// const useChannel = (channelName) => {
+//   const [channel, setChannel] = useState();
 
-  useEffect(() => {
-    let channel = socket.channel(channelName, {})
-    channel.join()
-      .receive("ok", resp => { 
-        console.log("Joined successfully", resp)
-        setChannel(channel);
-     })
-      .receive("error", resp => { console.log("Unable to join", resp) })
-      return () => {
-        channel.leave()
-      }
-  }, [])
+//   useEffect(() => {
+//     let channel = socket.channel(channelName, {})
+//     channel.join()
+//       .receive("ok", resp => { 
+//         console.log("Joined successfully", resp)
+//         setChannel(channel);
+//      })
+//       .receive("error", resp => { console.log("Unable to join", resp) })
+//       return () => {
+//         channel.leave()
+//       }
+//   }, [])
 
-    return [channel]
+//     return [channel]
 
-}
+// }
 
 function App() {
   const [channel] = useChannel('room:lobby');
