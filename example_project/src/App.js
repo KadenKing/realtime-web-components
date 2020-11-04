7c1f0e726b87182cfc91254aca0a1720d62fcf67
kaden king <kingk45@ufl.edu>
2020-11-04 14:09:03 -0500

    working

--- example_project/src/App.js
+++ example_project/src/App.js
@@ -1,12 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import RTTextInput from 'cra-rtc/dist/TextInput/RTTextInput'
+import jwt from 'jsonwebtoken'
+
+/**
+
+request that goes to developer's own server
+const authObj = request(user, password) {
+  return sign(user, date.now, SECRET)
+}
+
+**/
+
 
 function App() {
+  const token = jwt.sign({user: 'u1234'}, 'shh');
+
   return (
 
       <div className="App">
         <header className="App-header">
+          {JSON.stringify(token)}
           <RTTextInput id="text1"></RTTextInput>
         </header>
       </div>
