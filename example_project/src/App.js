8d60e108724517ea492bf715cd78c807bbab9bf5
Hubert Zhao <zhao.hubert@gmail.com>
2020-11-28 13:02:16 -0500

    made example project have other components to demo

--- example_project/src/App.js
+++ example_project/src/App.js
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 //import RTTextInput from 'cra-rtc/dist/TextInput/RTTextInput'
 import RTTextInput from 'cra-rtc/dist/TextInput/RTTextInput'
+import RTTextArea from 'cra-rtc/dist/TextInput/RTTextArea'
 import RTDraw from './RTDraw'
+import LikeCounter from 'cra-rtc/dist/NumericInput/LikeCounter'
 import jwt from 'jsonwebtoken'
 import CanvasDraw from "react-canvas-draw";
 /**
@@ -16,14 +18,15 @@ const authObj = request(user, password) {
 
 
 function App() {
-  const token = jwt.sign({user: 'u1234'}, 'shh');
+  //const token = jwt.sign({user: 'u1234'}, 'shh');
 
   return (
 
       <div className="App">
         <header className="App-header">
-          {JSON.stringify(token)}
-          <RTTextInput style={{marginBottom: "200px"}} id="text1"></RTTextInput>
+          <RTTextInput style={{marginBottom: "40px"}} id="text1"></RTTextInput>
+          <RTTextArea style={{marginBottom: "40px"}} id="textarea1"></RTTextArea>
+          <LikeCounter style={{marginBottom: "40px"}} id="like1"></LikeCounter>
           <RTDraw id="draw1"/>
         </header>
       </div>
